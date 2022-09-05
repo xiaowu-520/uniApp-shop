7102fe49d3805c147174573c6a2e5281bb512bc9
xiaowu <1059400336@qq.com>
2022-09-05 17:04:34 +0800

    完成首页的基本布局

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -5,36 +5,156 @@
         搜索
       </view>
     </view>
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <!-- 循环渲染轮播图的 item 项 -->
+      <swiper-item v-for="(item, i) in banners" :key="i" @click="goDetail(item.goods_id)">
+        <view class="swiper-item">
+          <!-- 动态绑定图片的 src 属性 -->
+          <image :src="item.image_src"></image>
+        </view>
+      </swiper-item>
+    </swiper>
+    <!-- 分类导航区域 -->
+    <view class="nav-list">
+      <view class="nav-item" v-for="(item, i) in navs" :key="i" @click="getTo(item)">
+        <image :src="item.image_src" class="nav-img"></image>
+      </view>
+    </view>
+    <!-- 楼层区域 -->
+    <view class="floor-list">
+      <!-- 楼层 item 项 -->
+      <view class="floor-item" v-for="(item, i) in floors" :key="i">
+        <!-- 楼层标题 -->
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+        <!-- 楼层图片区域 -->
+        <view class="floor-img-box">
+          <!-- 左侧大图片的盒子 -->
+          <view class="left-img-box">
+            <image class="left-img" :src="item.product_list[0].image_src"
+              :style="{width: item.product_list[0].image_width + 'rpx'}"></image>
+          </view>
+          <!-- 右侧 4 个小图片的盒子 -->
+          <view class="right-img-box">
+            <view class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0">
+              <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
+  import {
+    getBanners,
+    getNavList,
+    getFloor
+  } from '@/api/home.js'
   export default {
     data() {
       return {
-        
+        banners: [],
+        navs: [],
+        floors: []
       };
+    },
+    methods: {
+      async loadBanners() {
+        const res = await getBanners()
+        this.banners = res
+      },
+      async loadNavList() {
+        const res = await getNavList()
+        this.navs = res
+      },
+      async loadFloor() {
+        const res = await getFloor()
+        this.floors = res
+      },
+      goDetail(id) {
+        uni.navigateTo({
+          url: '/subpkgs/goods-detail/goods-detail?goods_id=' + id
+        })
+      },
+      // 跳转到相应页面
+      getTo(item) {
+        if (item.name === '分类') {
+          uni.switchTab({
+            url: '/pages/cate/cate'
+          })
+        }
+      }
+    },
+    onLoad() {
+      this.loadBanners()
+      this.loadNavList()
+      this.loadFloor()
     }
   }
 </script>
 
 <style lang="scss">
-.topSearch {
-  width: 750rpx;
-  height: 83px;
-  background-color: #c00000;
-  display: flex;
-  justify-content: center;
-  overflow: hidden;
-  .search_btn {
-    width: 400rpx;
-    height: 30px;
-    margin-top: 43px;
-    background-color: #fff;
-    border-radius: 10px;
+  .topSearch {
+    width: 750rpx;
+    height: 83px;
+    background-color: #c00000;
     display: flex;
     justify-content: center;
-    align-items: center;
+    overflow: hidden;
+
+    .search_btn {
+      width: 400rpx;
+      height: 30px;
+      margin-top: 43px;
+      background-color: #fff;
+      border-radius: 10px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
-}
+
+  swiper {
+    height: 330rpx;
+
+    .swiper-item,
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0;
+
+    .nav-img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
+
+  .floor-title {
+    height: 60rpx;
+    width: 100%;
+    display: flex;
+  }
+
+  .right-img-box {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+  .floor-img-box {
+    display: flex;
+    padding-left: 10rpx;
+
+    .left-img {
+      height: 392rpx !important;
+    }
+  }
 </style>
---
+++ subpkgs/goods-detail/goods-detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    good
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
