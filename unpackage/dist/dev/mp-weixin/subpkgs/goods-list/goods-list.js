8fc605e08a7ac8cfa6ea296fb12aaf0d87a9ba80
xiaowu <1059400336@qq.com>
2022-09-07 15:14:05 +0800

    1

---
+++ unpackage/dist/dev/mp-weixin/subpkgs/goods-list/goods-list.js
@@ -0,0 +1,33 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["subpkgs/goods-list/goods-list"],{
+
+/***/ 52:
+/*!********************************************************************************************************!*\
+  !*** C:/Users/10594/Desktop/黑马文件/uniApp/uni-shop/main.js?{"page":"subpkgs%2Fgoods-list%2Fgoods-list"} ***!
+  \********************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 4));
+var _goodsList = _interopRequireDefault(__webpack_require__(/*! ./subpkgs/goods-list/goods-list.vue */ 53));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;createPage(_goodsList.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 53:
+/*!*************************************************************************************!*\
+  !*** C:/Users/10594/Desktop/黑马文件/uniApp/uni-shop/subpkgs/goods-list/goods-list.vue ***!
+  \*************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\10594\\Desktop\\黑马文件\\uniApp\\uni-shop\\subpkgs\\goods-list\\goods-list.vue'");
+
+/***/ })
+
+},[[52,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/subpkgs/goods-list/goods-list.js.map
